b513058bded2da83126c7dcd8447464327baa4b0
Adrià <asolerc@campus.eug.es>
2020-03-11 13:41:53 +0100

    css

--- public/employeesList.php
+++ public/employeesList.php
@@ -8,20 +8,26 @@ $employeeslist = $employeesController->indexAction();
     <title>LlistaEmpleats</title>
     <link rel="stylesheet" type="text/css" href="employeesList.css">
 </head>
-<h1>Agenda Empleats</h1>
 
-<?php foreach($employeeslist as $employee){ ?>
-    <ul>
-        <li style="list-style-type:none"><img src="<?=$employee->getPhoto();?>"></li>
-        <li><?=$employee->getName();?></li>
-        <li><?=$employee->getDate();?></li>
-        <li><?=$employee->getPosition();?></li>
-        <li><?=$employee->getSalary();?></li>
-        <li style="list-style-type:none; display: block-inline">
-            <video controls poster="../assets/photo/cv.jpg">
-            <source src="<?=$employee->getCv();?>" type="video/mp4">
-            </video>
-        </li>
-    </ul>
-<?php } ?>
+<div class="container">
+    <div class="title">
+        <h1>Agenda Empleats</h1>
+    </div>
+    <div class="containerList">
+        <?php foreach($employeeslist as $employee){ ?>
+            <ul>
+                <li style="list-style-type:none"><img src="<?=$employee->getPhoto();?>"></li>
+                <li><?=$employee->getName();?></li>
+                <li><?=$employee->getDate();?></li>
+                <li><?=$employee->getPosition();?></li>
+                <li><?=$employee->getSalary();?></li>
+                <li style="list-style-type:none; display: block-inline">
+                    <video controls poster="../assets/photo/cv.jpg">
+                    <source src="<?=$employee->getCv();?>" type="video/mp4">
+                    </video>
+                </li>
+            </ul>
+        <?php } ?>
+    </div>
+</div>
 
